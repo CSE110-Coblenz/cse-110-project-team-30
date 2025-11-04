ef456fc56816f4a82d1abfd035d074873def590e
Britney Osgood <brielynn1504@gmail.com>
2025-11-03 20:24:53 -0800

    Bring in updated file

---
+++ frontend/src/PlayerModel.ts
@@ -0,0 +1,22 @@
+export class PlayerModel {
+  private totalPoints: number = 0;
+  private subscribers: ((points: number) => void)[] = [];
+
+  updateTotalPoints(points: number) {
+    this.totalPoints += points;
+    this.notify();
+  }
+
+  getTotalPoints() {
+    return this.totalPoints;
+  }
+
+  // Allow screens to subscribe to changes
+  subscribe(callback: (points: number) => void) {
+    this.subscribers.push(callback);
+  }
+
+  private notify() {
+    this.subscribers.forEach(callback => callback(this.totalPoints));
+  }
+}
