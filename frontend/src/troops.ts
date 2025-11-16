3fca1696df94258566153a7edbfe7f7e029437db
Ethan Pham <phamtethan@gmail.com>
2025-11-15 21:35:15 -0800

    Added troops.ts and moved card images to new folder in public

---
+++ frontend/src/troops.ts
@@ -0,0 +1,27 @@
+export const troops = {
+    
+    // Swordsmen
+    SwordsmanOne: { operation: "Addition", hp: 10, damage: 4, level: 1 },
+    SwordsmanTwo: { operation: "Addition", hp: 12, damage: 4, level: 2 },
+    SwordsmanThree: { operation: "Addition", hp: 14, damage: 5, level: 3 },
+    SwordsmanFour: { operation: "Addition", hp: 16, damage: 5, level: 4 },
+
+    //Archers
+    ArcherOne: { operation: "Subtraction", hp: 10, damage: 4, level: 1 },
+    ArcherTwo: { operation: "Subtraction", hp: 12, damage: 4, level: 2 },
+    ArcherThree: { operation: "Subtraction", hp: 14, damage: 5, level: 3 },
+    ArcherFour: { operation: "Subtraction", hp: 16, damage: 5, level: 4 },
+
+
+    //Spearmen
+    SpearmanOne: { operation: "Multiplication", hp: 14, damage: 5, level: 1 },
+    SpearmanTwo: { operation: "Multiplication", hp: 16, damage: 5, level: 2 },
+    SpearmanThree: { operation: "Multiplication", hp: 18, damage: 6, level: 3 },
+    SpearmanFour: { operation: "Multiplication", hp: 20, damage: 6, level: 4 },
+
+    //Cavalry
+    CavalryOne: { operation: "Division", hp: 14, damage: 5, level: 1 },
+    CavalryTwo: { operation: "Division", hp: 16, damage: 5, level: 2 },
+    CavalryThree: { operation: "Division", hp: 18, damage: 6, level: 3 },
+    CavalryFour: { operation: "Division", hp: 20, damage: 6, level: 4 },
+};
