28d771064e96ae2db3d0a155a6e785d044422760
Nathan Liu <nathanliu17@gmail.com>
2025-11-16 21:09:01 -0800

    add print on go backend start

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"cse-110-project-team-30/backend/internal/battle"
 	"cse-110-project-team-30/backend/internal/socket"
 	"cse-110-project-team-30/backend/routes"
+	"fmt"
 	"net/http"
 )
 
@@ -16,4 +17,5 @@ func main() {
 	go hub.Run() // goroutine
 	routes.RegisterBattleSocket(mux, hub)
 	http.ListenAndServe(":8080", mux)
+	fmt.Print("Starting server on :8080\n")
 }
