7864fb5eea436fcac03634c8f2789bcbb8b0c4f4
Yezi <2742380635@qq.com>
2025-11-25 10:50:44 -0800

    change frontend to make points can update to database

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,3 +1,49 @@
+// import type { Group } from "konva/lib/Group";
+
+// export interface View {
+//   getGroup(): Group;
+//   show(): void;
+//   hide(): void;
+// }
+
+// /**
+//  * Screen types for navigation
+//  *
+//  * - "login": Login screen
+//  * - "menu": Main menu screen
+//  * - "cards": Cards screen
+//  * - "battle": Battle screen
+//  * - "results": Results screen
+//  * - "leaderboard": Leaderboard screen
+//  * - pointsUpdate: Points earned/lost to display on results screen
+//  */
+// export type Screen =
+//   | { type: "login" }
+//   | { type: "menu" }
+//   | { type: "cards" }
+//   | { type: "battle" }
+//   | { type: "results"; pointsUpdate: number }
+//   | { type: "leaderboard" };
+
+// export abstract class ScreenController {
+//   abstract getView(): View;
+
+//   show(): void {
+//     this.getView().show();
+//   }
+
+//   hide(): void {
+//     this.getView().hide();
+//   }
+// }
+
+// export interface ScreenSwitcher {
+//   switchToScreen(screen: Screen): void;
+// }
+
+
+
+
 import type { Group } from "konva/lib/Group";
 
 export interface View {
@@ -12,17 +58,23 @@ export interface View {
  * - "login": Login screen
  * - "menu": Main menu screen
  * - "cards": Cards screen
+ * - "selection": Cards selection screen
  * - "battle": Battle screen
  * - "results": Results screen
  * - "leaderboard": Leaderboard screen
- * - pointsUpdate: Points earned/lost to display on results screen
+ * - cards: cards the user selects from the selection screen
  */
 export type Screen =
   | { type: "login" }
   | { type: "menu" }
   | { type: "cards" }
-  | { type: "battle" }
-  | { type: "results"; pointsUpdate: number }
+  | { type: "selection" }
+  | { type: "battle"; cards: string[] }
+  | { 
+      type: "results"; 
+      playerCrowns: number; 
+      enemyCrowns: number 
+    } 
   | { type: "leaderboard" };
 
 export abstract class ScreenController {
@@ -40,3 +92,26 @@ export abstract class ScreenController {
 export interface ScreenSwitcher {
   switchToScreen(screen: Screen): void;
 }
+
+export interface WSResponse {
+  tick: number;
+  troops: Troop[];
+}
+
+export interface Troop {
+  ID: number;
+  Type: string;
+  Health: number;
+  Team: number;
+  Position: Position;
+  Damage: number;
+  Speed: number;
+  Range: number;
+}
+
+export interface Position {
+  X: number;
+  Y: number;
+}
+
+export type Grid = Troop[][][];
